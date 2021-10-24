d700ffd19db259d67cbd5ed4241d5b4c02dce5ce
Justin Brickler <bricklerjustin@gmail.com>
2021-10-24 15:37:41 -0400

    Fixed not defined error

--- RelayControl.py
+++ RelayControl.py
@@ -2,6 +2,6 @@ from relay_lib_seeed import *
 
 def ChangeRelayState(relay_num, state):
     if (state):
-        relay_lib_seeed.relay_on(relay_num)
+        relay_on(relay_num)
     else:
-        relay_lib_seeed.relay_off(relay_num)
+        relay_off(relay_num)
